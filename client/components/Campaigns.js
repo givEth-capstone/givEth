c4aea26502a85a031fbf4a6176c9da7a862dd384
Farha Lina <farhalin@gmail.com>
2021-08-24 13:17:08 -0400

    fixed syntax error

--- client/components/Campaigns.js
+++ client/components/Campaigns.js
@@ -47,7 +47,7 @@ export default function Campaigns() {
     //   const campaigns = await axios.get('/api/campaigns')
     //   setCampaigns(campaigns)
     
-  }, [tag, campaigns])
+  {}}, [tag, campaigns])
 
   
 
