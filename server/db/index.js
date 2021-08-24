ea3ba9ef3cfb3b328b078703ad1ab4d863409b2c
Farha Lina <farhalin@gmail.com>
2021-08-24 14:56:43 -0400

    updated

--- server/db/index.js
+++ server/db/index.js
@@ -4,17 +4,10 @@ const db = require('./db')
 
 const User = require('./models/User')
 const Campaign = require('./models/Campaign')
-<<<<<<< HEAD
-
-Campaign.belongsTo(User)
-User.hasMany(Campaign)
-
-=======
 
 //associations could go here!
 Campaign.belongsTo(User)
 User.hasMany(Campaign)
->>>>>>> 09cfca4b1d4615ca5036f2b81f9eeb748abb3459
 
 module.exports = {
   db,
