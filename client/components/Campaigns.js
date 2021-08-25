bbbaa9df38ffdff53a45dfc0b59ddc8f5e9a0dc4
Nicky <puppy.nynz@gmail.com>
2021-08-25 16:41:43 -0400

    merging

--- client/components/Campaigns.js
+++ client/components/Campaigns.js
@@ -9,18 +9,61 @@ import MenuItem from '@material-ui/core/MenuItem';
 import FormControl from '@material-ui/core/FormControl';
 import axios from 'axios';
 import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+import { Grid } from '@material-ui/core';
+import { Link } from 'react-router-dom';
 
 const useStyles = makeStyles(() => ({
+  action: {
+    display: 'flex',
+    justifyContent: 'space-around',
+  },
+  container: {
+    display: 'flex',
+    justifyContent: 'space-evenly',
+    flexDirection: 'row',
+    maxWidth: '100vw',
+  },
+  content: {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
   formControl: {
     margin: 20,
     minWidth: 200,
     padding: 20,
     minWidth: 200,
-    position: 'absolute',
-    right: 20,
   },
-  selectEmpty: {
-    marginTop: 10,
+  gridContainer: {
+    justifyContent: 'flex-start',
+    display: 'flex',
+    alignItems: 'center',
+    alignContent: 'space-between',
+    margin: 20,
+    padding: 20,
+    flexFlow: 'row wrap',
+    spacing: 0
+  },
+  media: {
+    height: 270,
+    width: 270,
+    margin: '0 auto',
+  },
+  message: {
+    alignSelf: 'center'
+  },
+  root: {
+    borderRadius: 12,
+    width: 345,
+    height: 400,
+    textAlign: 'center',
+    margin: 10,
   },
 }));
 
@@ -44,16 +87,12 @@ export default function Campaigns() {
 
   useEffect(() => {
     async function fetchData() {
-      console.log(tag)
-      if (tag === 'All Campaigns') {
+      try {
         const response = await axios.get('/api/campaigns');
         setCampaigns(response.data);
-        console.log(campaigns);
-      } else {
-        //this call is failing
-        const response = await axios.get(`/api/campaigns/${tag}`);
-        setCampaigns(response.data);
-        console.log(campaigns);
+        setSelectedCampaigns(response.data);
+      } catch (err) {
+        console.log(err);
       }
     }
     fetchData();
@@ -84,33 +123,66 @@ export default function Campaigns() {
   // };
 
   return (
-    <div>
-      <FormControl className={classes.formControl}>
-        <InputLabel id='select-label'>
-          Select a tag to view campaigns
-        </InputLabel>
-        <Select
-          labelId='select-label'
-          id='campaign-select'
-          value={tag}
-          onChange={(event) => {
-            setTag(event.target.value);
-          }}
-        >
-          {tags.map((tag, i) => (
-            <MenuItem key={i} value={tag}>
-              {tag}
-            </MenuItem>
-          ))}
-        </Select>
-      </FormControl>
+    <div className={classes.container}>
       <div>
-        {!campaigns.length ? (
-          <h1>No Campaigns Yet</h1>
+        <FormControl className={classes.formControl}>
+          <InputLabel id='select-label'>
+            Select a tag to view campaigns
+          </InputLabel>
+          <Select
+            labelId='select-label'
+            id='campaign-select'
+            value={tag}
+            onChange={(event) => {
+              setTag(event.target.value);
+            }}
+          >
+            {tags.map((tag, i) => (
+              <MenuItem key={i} value={tag}>
+                {tag}
+              </MenuItem>
+            ))}
+          </Select>
+        </FormControl>
+      </div>
+
+      <div className={classes.gridContainer}>
+        {!selectedCampaigns.length ? (
+          <h1 className={classes.message}>No Campaigns Yet!</h1>
         ) : (
-          campaigns.map((campaign) => {
-            <Card></Card>;
-          })
+          <Grid container>
+            {selectedCampaigns.map((campaign) => {
+              return (
+                <Grid item key={campaign.id}>
+                  <Card className={classes.root}>
+                    <CardActionArea>
+                      <CardMedia
+                        component='img'
+                        alt='Campaign Image'
+                        height='140'
+                        className={classes.media}
+                        image={campaign.photoUrl}
+                        className={classes.media}
+                        // title={campaign.name}
+                      />
+                    </CardActionArea>
+                    <CardContent className={classes.content}>
+                      <Typography gutterBottom variant='h5' component='h2'>
+                        {campaign.name}
+                      </Typography>
+                    </CardContent>
+                    <CardActions className={classes.action}>
+                      <Link to={`/campaigns/${campaign.id}`}>
+                        <Button color='default' variant='contained'>
+                          See More
+                        </Button>
+                      </Link>
+                    </CardActions>
+                  </Card>
+                </Grid>
+              );
+            })}
+          </Grid>
         )}
       </div>
     </div>
