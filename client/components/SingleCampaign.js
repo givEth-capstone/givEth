48cd1e5b75b9b9995f25f2edabfe67b9f1f22c9d
Rachel Kahtan <kahtan.rachel@gmail.com>
2021-08-28 10:44:27 -0400

    style: center prog bar, add header to single card

--- client/components/SingleCampaign.js
+++ client/components/SingleCampaign.js
@@ -1,7 +1,7 @@
-import React, {useState, useEffect} from 'react'
-import axios from 'axios'
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
-import DonateButton from './DonateButton'
+import DonateButton from './DonateButton';
 
 import Card from '@material-ui/core/Card';
 import CardActionArea from '@material-ui/core/CardActionArea';
@@ -10,29 +10,35 @@ import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
-import { Grid } from '@material-ui/core'
+import { Grid } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
-import { LinearProgress } from '@material-ui/core/LinearProgress'
-
+import { LinearProgress } from '@material-ui/core/LinearProgress';
 
 const useStyles = makeStyles({
+  content: {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    flexDirection: 'column',
+  },
   root: {
-    maxWidth: '80%',
     flexGrow: 1,
-    height: '100%',
   },
   media: {
-    height: 280,
+    height: 400,
+    width: 600,
+    margin: '0 auto',
   },
   progress: {
     // width: '37.5%',
     float: 'left',
     borderRadius: '6px',
     height: '20px',
-    background: '#36B8E9', /* For browsers that do not support gradients */
+    background: '#36B8E9' /* For browsers that do not support gradients */,
     // background: '-webkit-linear-gradient(-90deg, #71F7F2, #36B8E9)', /* For Safari 5.1 to 6.0 */
     zIndex: 333,
-    boxShadow: 'inset 0 1px 2px rgba(0, 0, 0, 0.25), 0 1px rgba(255, 255, 255, 0.08)'
+    boxShadow:
+      'inset 0 1px 2px rgba(0, 0, 0, 0.25), 0 1px rgba(255, 255, 255, 0.08)',
   },
   glass: {
     width: '80%',
@@ -43,40 +49,39 @@ const useStyles = makeStyles({
     overflow: 'hidden',
     backgroundColor: '#dee0e0',
     boxShadow: '0 2px 3px rgba(0,0,0,.5) inset',
-  }
+  },
 });
 
 export default function SingleCampaign(props) {
-
   //const progress = props.campaign
   const campaignID = props.match.params.id;
   const classes = useStyles();
-  let [campaign, setCampaign] = React.useState([])
-  let [width, setWidth] = useState(0)
+  let [campaign, setCampaign] = React.useState([]);
+  let [width, setWidth] = useState(0);
 
   useEffect(() => {
     async function getCampaign(id) {
       try {
         const response = await axios.get(`/api/campaigns/${id}`);
         const data = response.data;
-        setCampaign(data)
+        setCampaign(data);
       } catch (err) {
         console.log(err);
       }
     }
-    getCampaign(campaignID)
+    getCampaign(campaignID);
   }, []);
 
   useEffect(() => {
     function getWidth() {
-      const received = campaign.received 
-      const needed = campaign.needed
-      const progress = (received/needed)*100
-      setWidth(progress)
+      const received = campaign.received;
+      const needed = campaign.needed;
+      const progress = (received / needed) * 100;
+      setWidth(progress);
     }
-    getWidth()
+    getWidth();
   }, [campaign]);
-  
+
   return (
     <div>
       {campaign.length < 1 ? (
@@ -84,11 +89,16 @@ export default function SingleCampaign(props) {
       ) : (
         <Grid
           container
-          direction="column"
-          alignItems="center"
-          justifyContent="center"
+          direction='column'
+          alignItems='center'
+          justifyContent='center'
         >
           <Card className={classes.root}>
+            <CardContent className={classes.content}>
+              <Typography gutterBottom variant='h5' component='h2'>
+                {campaign.name}
+              </Typography>
+            </CardContent>
             <CardActionArea>
               <CardMedia
                 className={classes.media}
@@ -96,22 +106,25 @@ export default function SingleCampaign(props) {
                 title={campaign.title}
               />
 
-              <CardContent>
-                <Typography gutterBottom variant="h5" component="h2">
+              <CardContent className={classes.content}>
+                <Typography gutterBottom variant='h5' component='h2'>
                   {campaign.title}
                 </Typography>
-                <Typography gutterBottom variant="h5" component="h2">
+                <Typography gutterBottom variant='h5' component='h2'>
                   {campaign.info}
                 </Typography>
-                <Typography gutterBottom variant="h5" component="h2">
+                <Typography gutterBottom variant='h5' component='h2'>
                   Goal: {campaign.needed} ETHER
                 </Typography>
 
                 <div className={classes.glass}>
-                  <div className={classes.progress} style={{width: `${width}%`}}></div>
+                  <div
+                    className={classes.progress}
+                    style={{ width: `${width}%` }}
+                  ></div>
                 </div>
 
-                <Typography gutterBottom variant="h5" component="h2">
+                <Typography gutterBottom variant='h5' component='h2'>
                   {/* Amount raised: {campaign.amountRaised/campaign.needed} */}
                 </Typography>
               </CardContent>
