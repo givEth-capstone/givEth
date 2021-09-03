fb59a1317b5ad63f0d3ff7f7f68f9d51dd4cf4c9
Amber Abreu <ambermabreu@gmail.com>
2021-09-03 16:15:49 -0400

    Style: Hamburger Menu

--- client/components/Navbar.js
+++ client/components/Navbar.js
@@ -1,9 +1,15 @@
-import React from 'react';
+import React , {useState} from 'react';
 import { connect } from 'react-redux';
 import { Link, Redirect } from 'react-router-dom';
 import { logout } from '../store';
 import { makeStyles } from '@material-ui/core/styles';
 import Typography from '@material-ui/core/Typography';
+import useMediaQuery from '@material-ui/core/useMediaQuery';
+import MenuIcon from '@material-ui/icons/Menu';
+import Menu from '@material-ui/core/Menu';
+import MenuItem from '@material-ui/core/MenuItem';
+
+  
 
 
 const useStyles = makeStyles(() => ({
@@ -12,10 +18,11 @@ const useStyles = makeStyles(() => ({
     display: 'flex',
     justifyContent: 'space-between',
     alignItems: 'center',
-    flexDirection: 'row',
+    flexDirection:  'row',
     maxWidth: '100vw',
     alignContent: 'space-between',
     height: 80,
+    zIndex: 100
 
   },
   img: {
@@ -29,67 +36,122 @@ const useStyles = makeStyles(() => ({
     display: 'flex',
     alignItems: 'center',
   },
+  Bars: {
+    display: 'block',
+    color: 'white',
+    position: 'absolute',
+    top: 0,
+    right: 0,
+    transform: 'translate(-100%, 75%)',
+    fontSize: '1.8rem',
+    cursor: 'pointer'
+  }
 }));
 
-const Navbar = ({ handleClick, isLoggedIn }) => {
-  const classes = useStyles();
-  return (
-    <div>
-      <nav className={classes.container}>
-        <img className={classes.img} src='https://i.imgur.com/hPqLWJx.png'/>
-        <div className={classes.navLinks}>
-          {isLoggedIn ? (
-            <div>
+
+const NavLinks = (props) => {
+  const matches = useMediaQuery('(max-width:800px)');
+ return (
+  <div style={{display:'flex', flexDirection: matches ? 'column' : 'row', top:80 }}>
+    {/* need help!! want it to be column when it is clicked. */}
+              
               <Link to='/'>
-                <Typography className={classes.link}>Home</Typography>
+                <Typography className={props.classes.link}>Home</Typography>
               </Link>
               <Link to='/createCampaign'>
-                <Typography className={classes.link}>
+                <Typography className={props.classes.link}>
                   Start A Campaign
                 </Typography>
               </Link>
               {/*Link to Donate// Can link to All campaigns with header that says Choose a campaign to donate to */}
               <Link to='/campaigns'>
-                <Typography className={classes.link}>Donate</Typography>
+                <Typography className={props.classes.link}>Donate</Typography>
               </Link>
               <Link to='/about'>
-                <Typography className={classes.link}>About</Typography>
+                <Typography className={props.classes.link}>About</Typography>
               </Link>
               <Link to='/profile'>
-                <Typography className={classes.link}>Profile</Typography>
-              </Link>
-              <a href='#' onClick={handleClick}>
-                <Typography className={classes.link}>Logout</Typography>
-              </a>
-            </div>
-          ) : (
-            <div>
-              {/* Don't display logout button if not logged in! */}
-              <Link to='/'>
-                <Typography className={classes.link}>Home</Typography>
-              </Link>
-              <Link to='/createCampaign'>
-                <Typography className={classes.link}>
-                  Start A Campaign
-                </Typography>
+                <Typography className={props.classes.link}>Profile</Typography>
               </Link>
 
-              <Link to='/campaigns'>
-                <Typography className={classes.link}>Donate</Typography>
-              </Link>
-              <Link to='/about'>
-                <Typography className={classes.link}>About</Typography>
-              </Link>
-              <Link to='/profile'>
-                <Typography className={classes.link}>Profile</Typography>
-              </Link>
+              {props.loggedIn ? <a href='#' onClick={props.handleClick}>
+                <Typography className={props.classes.link}>Logout</Typography>
+              </a> : <> </>}
+
             </div>
-          )}
+ )
+}
+
+//style={{flexDirection: matches ? 'column' : 'row'}}
+
+const MenuBar = (props) => {
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+  return (
+    <div >
+      <MenuIcon className={props.classes.Bars} onClick={handleClick}/>
+      <Menu
+      id="simple-menu"
+      anchorEl={anchorEl}
+      keepMounted
+      open={Boolean(anchorEl)}
+      onClose={handleClose}
+    >
+      <MenuItem onClick={handleClose}> <Link to='/'>Home</Link>  </MenuItem>
+      <MenuItem onClick={handleClose}> <Link to='/createCampaign'>Start a Campaign</Link> </MenuItem>
+      <MenuItem onClick={handleClose}> <Link to='/campaigns'>Donate</Link> </MenuItem>
+      <MenuItem onClick={handleClose}> <Link to='/about'>About</Link> </MenuItem>
+      <MenuItem onClick={handleClose}> <Link to='/profile'>Profile</Link> </MenuItem>
+      {props.loggedIn && <MenuItem onClick={handleClose}> <a href='#' onClick={props.handleLogout}>Logout</a> </MenuItem>}
+    </Menu>
+    </div>
+  )
+}
+
+
+
+const Navbar = ({ handleClick, isLoggedIn }) => {
+  const classes = useStyles();
+  const matches = useMediaQuery('(max-width:800px)');
+  
+  
+  return (
+    <div>
+      
+      <nav className={classes.container} >
+        <img className={classes.img} src='https://i.imgur.com/hPqLWJx.png'/>
+        <div className={classes.navLinks}>
+          
+        
+          {
+          isLoggedIn ? 
+          (
+            matches ? ( <MenuBar loggedIn={true} classes={classes} handleLogout={handleClick}/> ) :
+            
+           <NavLinks loggedIn={true} handleClick={handleClick} classes={classes} />
+          )
+
+           : (
+             matches ? (<MenuBar loggedIn={false} classes={classes}/> ) : 
+            <NavLinks loggedIn={false} handleClick={handleClick} classes={classes}/>
+          )
+          }
+
+
         </div>
+      
       </nav>
+
     </div>
   );
 };
+
+
 
 /**
  * CONTAINER
@@ -109,3 +171,4 @@ const mapDispatch = (dispatch) => {
 };
 
 export default connect(mapState, mapDispatch)(Navbar);
+
